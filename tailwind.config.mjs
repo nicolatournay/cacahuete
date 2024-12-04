6d498972cc3c6109b514c6466e193944dc0a84cb
Nicola <nicolatournay@outlook.com>
2024-12-04 04:55:31 +0100

    daisyui + homepage style added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,5 +12,11 @@ export default {
     		colors: {}
     	}
     },
-	plugins: [require("tailwindcss-animate")],
+	plugins: [
+		require("tailwindcss-animate"),
+		require("daisyui")
+	],
+	daisyui: {
+		themes: ["retro"],
+	}
 }
